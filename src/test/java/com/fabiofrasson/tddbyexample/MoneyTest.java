235bbe1cc61452d36510a943efb55359ffbdd04b
Fabio Frasson <fabio.frass@gmail.com>
2021-09-02 19:34:42 -0300

    Equality and inequality tests

--- src/test/java/com/fabiofrasson/tddbyexample/MoneyTest.java
+++ src/test/java/com/fabiofrasson/tddbyexample/MoneyTest.java
@@ -3,6 +3,7 @@ package com.fabiofrasson.tddbyexample;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MoneyTest {
 
@@ -13,5 +14,11 @@ public class MoneyTest {
     assertEquals(10, product.amount);
     product = five.times(3);
     assertEquals(15, product.amount);
+  }
+
+  @Test
+  void testEquality() {
+    assertEquals(new Dollar(5), new Dollar(5));
+    assertNotEquals(new Dollar(5), new Dollar(8));
   }
 }
