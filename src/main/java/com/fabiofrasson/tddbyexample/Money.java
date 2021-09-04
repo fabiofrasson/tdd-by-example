4f5a7ac314cb301503969c487c11f261a25e3f59
Fabio Frasson <fabio.frass@gmail.com>
2021-09-04 10:31:46 -0300

    Dollar/Franc references in test class removed

--- src/main/java/com/fabiofrasson/tddbyexample/Money.java
+++ src/main/java/com/fabiofrasson/tddbyexample/Money.java
@@ -1,8 +1,18 @@
 package com.fabiofrasson.tddbyexample;
 
-public class Money {
+public abstract class Money {
 
   protected int amount;
+
+  public abstract Money times(int multiplier);
+
+  public static Money dollar(int amount) {
+    return new Dollar(amount);
+  }
+
+  public static Money franc(int amount) {
+    return new Franc(amount);
+  }
 
   public boolean equals(Object object) {
     Money money = (Money) object;
